5b3e64fe8b94041349da37764ff59764e0011a17
maggie3000 <72889935+maggie3000@users.noreply.github.com>
2021-03-18 19:07:25 -0700

    created landing_page.html

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/landing_page', methods=["GET", "POST"])
 def landing_page():
-    return render_template("Landing_Page.html")
+    return render_template("landing_page.html")
 
-@app.route('/')
-def home_page():
-    return render_template("index.html")
 
 @app.route('/timeline/post')
 def individual_post():
-
     return render_template("individualPost.html")
+
+if __name__ == "__main__":
+    app.run(port='80', host='127.0.0.1')
